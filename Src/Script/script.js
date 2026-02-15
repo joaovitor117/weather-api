8606868917d0f8dfc4a2d48e863ab20e11b0c187
joaovitor117 <70237799+joaovitor117@users.noreply.github.com>
2026-02-15 20:37:11 -0300

    Add files via upload

--- Src/Script/script.js
+++ Src/Script/script.js
@@ -6,46 +6,80 @@ const errorMsg = document.querySelector("#errorBox")
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
-
     const cidade = input.value.trim();
     const apiKey = "1424c75b00084e39fb7d635ab5e78523";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cidade)}&appid=${apiKey}&units=metric&lang=pt_br`;
 
     console.log("Cidade buscada:", cidade); 
 
     try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error("Cidade não encontrada");
-        } else {
-            errorMsg.classList.remove("show")
-            errorMsg.classList.add("hide")
-            weather.classList.remove("hide")
-            weather.classList.add("show")
-        }
-
-        const data = await response.json();
-        weather.querySelector(".error")?.remove();
+        const responseWeather = await fetch( 
+            `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cidade)}&appid=${apiKey}&units=metric&lang=pt_br` ); 
+            if (!responseWeather.ok) throw new Error("Cidade não encontrada"); 
+            const dataWeather = await responseWeather.json();
 
         document.getElementById("search").classList.add("show");
 
-        document.getElementById("title").textContent = `${data.name}, ${data.sys.country}`;
-        document.getElementById("temp_value").textContent = `${Math.round(data.main.temp)} °C`;
-        document.getElementById("temp_description").textContent = data.weather[0].description;
-        document.getElementById("max_temp").textContent = `${Math.round(data.main.temp_max)} °C`;
-        document.getElementById("wind").textContent = `${data.wind.speed} km/h`;
-        document.getElementById("clouds").textContent = `${data.clouds.all}%`;
-        document.getElementById("humidity").textContent = `${data.main.humidity}%`;
+        document.getElementById("title").textContent = `${dataWeather.name}, ${dataWeather.sys.country}`;
+        document.getElementById("temp_value").textContent = `${Math.round(dataWeather.main.temp)} °C`;
+        document.getElementById("temp_description").textContent = dataWeather.weather[0].description;
+        document.getElementById("max_temp").textContent = `${Math.round(dataWeather.main.temp_max)} °C`;
+        document.getElementById("wind").textContent = `${dataWeather.wind.speed} km/h`;
+        document.getElementById("clouds").textContent = `${dataWeather.clouds.all}%`;
+        document.getElementById("humidity").textContent = `${dataWeather.main.humidity}%`;
 
-        const icon = data.weather[0].icon;
+        const icon = dataWeather.weather[0].icon;
         document.getElementById("temp_img").src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
-        if (data.clouds.all >= 50) {
+        if (dataWeather.clouds.all >= 50) {
             document.querySelector("body").classList.add("nuvem")
         } else {
             document.querySelector("body").classList.remove("nuvem")
         }
 
+        const responseForecast = await fetch( 
+            `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURI(cidade)}&appid=${apiKey}&units=metric&lang=pt_br`
+        );
+        const dataForecast = await responseForecast.json();
+
+        const hourContainer = document.getElementById("hourForecast_container")
+        hourContainer.innerHTML = "";
+        dataForecast.list.slice(0, 5).forEach(item => {
+            const div = document.createElement("div")
+            div.classList.add("info")
+            const hora = new Date(item.dt * 1000).getHours().toString().padStart(2, "0");
+            div.innerHTML = ` 
+            <h5>${hora}:00</h5> 
+            <img src="http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png" alt="clima"> 
+            <h5>${Math.round(item.main.temp)} °C</h5> `;
+            hourContainer.appendChild(div);
+        }); 
+            const dayContainer = document.getElementById("dayForecast_container"); 
+            dayContainer.innerHTML = "";
+            const days = {}; 
+            dataForecast.list.forEach(item => { 
+                const date = new Date(item.dt * 1000);
+                const dia = date.toLocaleDateString("pt-BR", { day: "2-digit", month: "short" }); 
+                const hora = date.getHours();
+                if (hora === 12 && !days[dia]) { 
+                    days[dia] = item;
+                } 
+            });
+
+        Object.entries(days).slice(0, 5).forEach(([dia, item]) => { 
+            const div = document.createElement("div"); 
+            div.classList.add("info"); 
+            div.innerHTML = ` 
+            <h5>${dia}</h5> 
+            <img src="http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png" alt="clima"> 
+            <h5>${Math.round(item.main.temp)} °C</h5> `
+            ;
+            dayContainer.appendChild(div);
+        });
+
+        errorMsg.classList.remove("show")
+        errorMsg.classList.add("hide")
+        weather.classList.remove("hide")
+        weather.classList.add("show")
         input.value = "";
     } catch (error) {
         console.log("Cidade não encontrada");
